a5332582d5f6efe965ea7bfcbd9891176bd874dc
NDeleo <n.deleo94@gmail.com>
2023-04-24 00:40:40 -0300

    Se agrega serializer, api y uris de facultades

--- apps/fw/routers.py
+++ apps/fw/routers.py
@@ -1,9 +1,11 @@
 from rest_framework import routers
 
 from apps.fw.api.UniversidadAPI import UniversidadAPIView
+from apps.fw.api.FacultadAPI import FacultadAPIView
 
 router = routers.DefaultRouter()
 
 router.register("universidades", UniversidadAPIView)  # GET (List, Retrieve)
+router.register("facultades", FacultadAPIView)  # GET (List, Retrieve)
 
 urlpatterns = router.urls
--- apps/fw/urls.py
+++ apps/fw/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 
 from apps.fw.uris.UniversidadURLs import urlUniversidad
+from apps.fw.uris.FacultadURLs import urlFacultad
 
-urlpatterns = [
-    # urlUniversidad
-]
+urlpatterns = []  # urlUniversidad + urlFacultad
