6ce7bcbda84fc06442fbcee1db44528a9b6fcd63
NDeleo <n.deleo94@gmail.com>
2023-04-27 21:06:41 -0300

    Se agrega serializers, apis y urls para puesto

--- apps/fw/routers.py
+++ apps/fw/routers.py
@@ -5,6 +5,7 @@ from apps.fw.api.FacultadAPI import FacultadAPIView
 from apps.fw.api.CarreraAPI import CarreraAPIView
 from apps.fw.api.TituloAPI import TituloAPIView
 from apps.fw.api.OrganizacionAPI import OrganizacionAPIView
+from apps.fw.api.PuestoAPI import PuestoAPIView
 
 router = routers.DefaultRouter()
 
@@ -13,5 +14,6 @@ router.register("facultades", FacultadAPIView)  # GET (List, Retrieve)
 router.register("carreras", CarreraAPIView)  # GET (List, Retrieve)
 router.register("titulos", TituloAPIView)  # GET (List, Retrieve)
 router.register("organizaciones", OrganizacionAPIView)  # GET (List, Retrieve), POST
+router.register("puestos", PuestoAPIView)  # GET (List, Retrieve), POST
 
 urlpatterns = router.urls
---
+++ apps/fw/uris/PuestoURLs.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+from apps.fw.api.PuestoAPI import *
+
+urlPuesto = [
+    path(
+        "api/puestos/",
+        PuestoListAPIView.as_view({"get": "list"}),
+        name="puesto_list",
+    ),
+    path(
+        "api/puestos/<int:pk>/",
+        PuestoDetailAPIView.as_view({"get": "retrieve"}),
+        name="puesto_detail",
+    ),
+]
