e52e5c2ddeffc575e52d6f611215d70cc830c90b
NDeleo <n.deleo94@gmail.com>
2023-05-05 00:07:12 -0300

    Se renombrar archivos urls y se utiliza ruta absoluta para imports

--- apps/fw/urls.py
+++ apps/fw/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 
-from apps.fw.uris.UniversidadURLs import urlUniversidad
-from apps.fw.uris.FacultadURLs import urlFacultad
-from apps.fw.uris.CarreraURLs import urlCarrera
-from apps.fw.uris.TituloURLs import urlTitulo
-from apps.fw.uris.OrganizacionURLs import urlOrganizacion
-from apps.fw.uris.PuestoURLs import urlPuesto
-from apps.fw.uris.ActividadURLs import urlActividad
-from apps.fw.uris.EgresoURLs import urlEgreso
+from apps.fw.uris.universidad_urls import urlUniversidad
+from apps.fw.uris.facultad_urls import urlFacultad
+from apps.fw.uris.carrera_urls import urlCarrera
+from apps.fw.uris.titulo_urls import urlTitulo
+from apps.fw.uris.organizacion_urls import urlOrganizacion
+from apps.fw.uris.puesto_urls import urlPuesto
+from apps.fw.uris.actividad_urls import urlActividad
+from apps.fw.uris.egreso_urls import urlEgreso
 
 urls = (
     urlUniversidad
