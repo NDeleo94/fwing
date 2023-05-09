3050e45ef43941bbc1173c8c01a5da044403789c
NDeleo <n.deleo94@gmail.com>
2023-05-08 22:28:27 -0300

    Se configura vista admin para carrera

--- apps/fw/admin/carrera_admin.py
+++ apps/fw/admin/carrera_admin.py
@@ -2,5 +2,34 @@ from django.contrib import admin
 
 from apps.fw.models.carrera_model import Carrera, Titulo
 
-admin.site.register(Carrera)
-admin.site.register(Titulo)
+
+class TituloInline(admin.TabularInline):
+    model = Titulo
+    extra = 1
+
+
+@admin.register(Carrera)
+class CarreraAdmin(admin.ModelAdmin):
+    inlines = [
+        TituloInline,
+    ]
+    list_display = ("carrera", "facultad", "estado")
+    fieldsets = (
+        (
+            "Datos",
+            {
+                "fields": (
+                    "carrera",
+                    "facultad",
+                    "web",
+                    "plan",
+                    "acreditacion",
+                    "acreditadora",
+                    "estado",
+                )
+            },
+        ),
+    )
+    list_filter = ["estado"]
+
+    search_fields = ("carrera",)
