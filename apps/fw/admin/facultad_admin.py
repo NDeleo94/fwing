5c699e23cf7c1a56f2f8f84bfca63f61bb0d7169
NDeleo <n.deleo94@gmail.com>
2023-05-08 22:22:24 -0300

    Se configura vista admin para facultad

--- apps/fw/admin/facultad_admin.py
+++ apps/fw/admin/facultad_admin.py
@@ -2,4 +2,37 @@ from django.contrib import admin
 
 from apps.fw.models.facultad_model import Facultad
 
-admin.site.register(Facultad)
+
+@admin.register(Facultad)
+class FacultadAdmin(admin.ModelAdmin):
+    list_display = ("facultad", "universidad", "estado")
+    fieldsets = (
+        (
+            "Datos",
+            {
+                "fields": (
+                    "facultad",
+                    "acronimo",
+                    "domicilio",
+                    "universidad",
+                    "web",
+                )
+            },
+        ),
+        (
+            "Contacto",
+            {
+                "fields": (
+                    "email",
+                    "telefono",
+                )
+            },
+        ),
+        ("Permisos", {"fields": ("estado",)}),
+    )
+    list_filter = ["estado"]
+
+    search_fields = (
+        "facultad",
+        "acronimo",
+    )
