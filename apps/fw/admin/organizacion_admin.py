d343c7d343d17be6babf1f742cd67c912753bbb0
NDeleo <n.deleo94@gmail.com>
2023-05-08 22:39:35 -0300

    Se configura vista admin para organizacion

--- apps/fw/admin/organizacion_admin.py
+++ apps/fw/admin/organizacion_admin.py
@@ -2,4 +2,39 @@ from django.contrib import admin
 
 from apps.fw.models.organizacion_model import Organizacion
 
-admin.site.register(Organizacion)
+
+@admin.register(Organizacion)
+class OrganizacionAdmin(admin.ModelAdmin):
+    list_display = (
+        "organizacion",
+        "tipo",
+        "estado",
+    )
+    fieldsets = (
+        (
+            "Datos",
+            {
+                "fields": (
+                    "organizacion",
+                    "tipo",
+                    "descripcion",
+                )
+            },
+        ),
+        (
+            "Contacto",
+            {
+                "fields": (
+                    "email",
+                    "web",
+                )
+            },
+        ),
+        ("Permisos", {"fields": ("estado",)}),
+    )
+    search_fields = ("organizacion",)
+
+    list_filter = [
+        "estado",
+        "tipo",
+    ]
