b6dfa1c2dbc2991ddd3d34d31a33899dc8de1723
NDeleo <n.deleo94@gmail.com>
2023-05-19 21:09:03 -0300

    Se actualiza urls de egresado

--- apps/fw/uris/egresado_urls.py
+++ apps/fw/uris/egresado_urls.py
@@ -5,12 +5,12 @@ from apps.fw.api.egresado_api import *
 urlEgresado = [
     path(
         "api/egresados/",
-        EgresadoListAPIView.as_view({"get": "list"}),
+        EgresadoReadOnlyAPIView.as_view({"get": "list"}),
         name="egresado_list",
     ),
     path(
         "api/egresados/<int:pk>/",
-        EgresadoDetailAPIView.as_view({"get": "retrieve"}),
+        EgresadoReadOnlyAPIView.as_view({"get": "retrieve"}),
         name="egresado_detail",
     ),
 ]
