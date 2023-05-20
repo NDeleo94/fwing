0042a783414fb6e13eafc1b86cf948203b367849
NDeleo <n.deleo94@gmail.com>
2023-05-20 02:08:35 -0300

    Se actualiza apiview de egresado

--- apps/fw/api/egresado_api.py
+++ apps/fw/api/egresado_api.py
@@ -1,8 +1,28 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 
 from apps.fw.serializers.egresado_serializers import *
 
 
-class EgresadoReadOnlyAPIView(viewsets.ReadOnlyModelViewSet):
-    serializer_class = EgresadoReadOnlySerializer
-    queryset = serializer_class.Meta.model.objects.filter(is_active=True)
+class EgresadoViewSet(
+    mixins.UpdateModelMixin,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    viewsets.GenericViewSet,
+):
+    """
+    A viewset that provides `retrieve`, `update`, and `list` actions.
+
+    To use it, override the class and set the `.queryset` and
+    `.serializer_class` attributes.
+    """
+
+    pass
+
+
+class EgresadoAPIView(EgresadoViewSet):
+    queryset = FwUser.objects.filter(is_active=True)
+
+    def get_serializer_class(self):
+        if self.action == "update":
+            return EgresadoUpdateSerializer
+        return EgresadoReadOnlySerializer
