dc6d1e19a94e1266a0b9749a8a7e3ec45ec863cf
NDeleo <n.deleo94@gmail.com>
2023-05-26 13:04:12 -0300

    Se actualiza la creaciond de superusuario

--- apps/fw/models/user_model.py
+++ apps/fw/models/user_model.py
@@ -8,27 +8,27 @@ from django.contrib.auth.models import (
 
 
 class FwUserManager(BaseUserManager):
-    def create_user(self, email, password=None):
+    def create_user(self, dni, password=None):
         """
-        Creates and saves a User with the given email and password.
+        Creates and saves a User with the given dni and password.
         """
-        if not email:
-            raise ValueError("Users must have an email address")
+        if not dni:
+            raise ValueError("Users must have an dni")
 
         user = self.model(
-            email=self.normalize_email(email),
+            dni=dni,
         )
 
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password=None):
+    def create_superuser(self, dni, password=None):
         """
-        Creates and saves a superuser with the given email and password.
+        Creates and saves a superuser with the given dni and password.
         """
         user = self.create_user(
-            email,
+            dni,
             password=password,
         )
         user.is_admin = True
