e84ccc3fcbc8d676739c4cfc28a905491ac55fc9
NDeleo <n.deleo94@gmail.com>
2023-06-03 20:59:24 -0300

    Se actualiza el metodo post

--- apps/fw/views/siu_views.py
+++ apps/fw/views/siu_views.py
@@ -16,23 +16,44 @@ class EgresadosSIU(APIView):
         result = [egresado["post"] for egresado in egresados]
         return result
 
-    def getAtributosEgresado(self, egresado):
-        return (
-            egresado.get("apellido").title(),
-            egresado.get("nombres").title(),
-            egresado.get("email"),
-            egresado.get("fecha_nacimiento"),
-            egresado.get("nacionalidad").title(),
-            egresado.get("localidadnac").title(),
-            egresado.get("localidad").title(),
-            egresado.get("domicilio").title(),
-            egresado.get("certificado").title(),
-            egresado.get("sexo"),
+    def crearEgresado(self, dni, carrera, egresado):
+        apellidos_nuevo_egresado = egresado.get("apellido").title()
+        nombres_nuevo_egresado = egresado.get("nombres").title()
+        email_nuevo_egresado = egresado.get("email")
+        fecha_nac_nuevo_egresado = egresado.get("fecha_nacimiento")
+        nacionalidad_nuevo_egresado = egresado.get("nacionalidad").title()
+        ciudad_natal_nuevo_egresado = egresado.get("localidadnac").title()
+        ciudad_actual_nuevo_egresado = egresado.get("localidad").title()
+        domicilio_nuevo_egresado = egresado.get("domicilio").title()
+        certificado_nuevo_egresado = egresado.get("certificado").title()
+        sexo_nuevo_egresado = egresado.get("sexo")
+
+        nuevo_egresado = FwUser.objects.create(
+            dni=dni,
+            apellidos=apellidos_nuevo_egresado,
+            nombres=nombres_nuevo_egresado,
+            email=email_nuevo_egresado,
+            fecha_nac=fecha_nac_nuevo_egresado,
+            nacionalidad=nacionalidad_nuevo_egresado,
+            ciudad_natal=ciudad_natal_nuevo_egresado,
+            ciudad_actual=ciudad_actual_nuevo_egresado,
+            domicilio=domicilio_nuevo_egresado,
+            certificado=certificado_nuevo_egresado,
+            sexo=sexo_nuevo_egresado,
+        )
+
+        ciclo_egreso_nuevo_egresado = egresado.get("fecha_egreso")
+
+        Egreso.objects.create(
+            carrera=carrera,
+            usuario=nuevo_egresado,
+            ciclo_egreso=ciclo_egreso_nuevo_egresado,
         )
 
     def post(self, request):
         egresados = self.getEgresados()
         contador_nuevos_egresados = 0
+
         for egresado in egresados:
             carrera_nuevo_egresado = egresado.get("carrera")
 
@@ -45,41 +66,11 @@ class EgresadosSIU(APIView):
                 usuario = FwUser.objects.filter(dni=dni_nuevo_egresado).exists()
 
                 if not usuario:
-                    (
-                        apellidos_nuevo_egresado,
-                        nombres_nuevo_egresado,
-                        email_nuevo_egresado,
-                        fecha_nac_nuevo_egresado,
-                        nacionalidad_nuevo_egresado,
-                        ciudad_natal_nuevo_egresado,
-                        ciudad_actual_nuevo_egresado,
-                        domicilio_nuevo_egresado,
-                        certificado_nuevo_egresado,
-                        sexo_nuevo_egresado,
-                    ) = self.getAtributosEgresado(egresado)
-
-                    nuevo_egresado = FwUser.objects.create(
+                    self.crearEgresado(
                         dni=dni_nuevo_egresado,
-                        apellidos=apellidos_nuevo_egresado,
-                        nombres=nombres_nuevo_egresado,
-                        email=email_nuevo_egresado,
-                        fecha_nac=fecha_nac_nuevo_egresado,
-                        nacionalidad=nacionalidad_nuevo_egresado,
-                        ciudad_natal=ciudad_natal_nuevo_egresado,
-                        ciudad_actual=ciudad_actual_nuevo_egresado,
-                        domicilio=domicilio_nuevo_egresado,
-                        certificado=certificado_nuevo_egresado,
-                        sexo=sexo_nuevo_egresado,
-                    )
-
-                    ciclo_egreso_nuevo_egresado = egresado.get("fecha_egreso")
-
-                    Egreso.objects.create(
                         carrera=carrera,
-                        usuario=nuevo_egresado,
-                        ciclo_egreso=ciclo_egreso_nuevo_egresado,
+                        egresado=egresado,
                     )
-
                     contador_nuevos_egresados += 1
 
         return Response(
