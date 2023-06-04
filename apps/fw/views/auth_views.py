764436b241fe60969035bc49b0ddcbff401f6204
NDeleo <n.deleo94@gmail.com>
2023-06-04 20:32:02 -0300

    Se agrega login con google

--- apps/fw/views/auth_views.py
+++ apps/fw/views/auth_views.py
@@ -7,6 +7,25 @@ from rest_framework.authtoken.models import Token
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
 
+from apps.fw.models.user_model import FwUser
+
+
+class LoginGoogleView(APIView):
+    def post(self, request):
+        google_email = request.data.get("email")
+
+        if google_email:
+            user = FwUser.objects.filter(email=google_email).first()
+
+            if user:
+                # Generate or retrieve the token for the user
+                token, _ = Token.objects.get_or_create(user=user)
+                return Response({"token": token.key})
+            else:
+                return Response({"error": "Invalid credentials"}, status=400)
+        else:
+            return Response({"error": "Missing email"}, status=400)
+
 
 class LoginView(APIView):
     def post(self, request):
