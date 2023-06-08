efe87db6fa587d9538ddbe87f54cb3a218978730
NDeleo <n.deleo94@gmail.com>
2023-06-08 00:18:00 -0300

    Se corrige apiview de imagen

--- apps/fw/views/image_views.py
+++ apps/fw/views/image_views.py
@@ -8,14 +8,14 @@ from rest_framework.response import Response
 
 class ImageView(APIView):
     def get(self, request, image_path):
+        dot_index = image_path.rindex(".") + 1
+        ext = image_path[dot_index:]
         try:
             image_file = open(
                 os.path.join(settings.MEDIA_ROOT, f"images/{image_path}"), "rb"
             )
             response = FileResponse(image_file)
-            response[
-                "Content-Type"
-            ] = "image/*"  # Replace with the appropriate content type for your image
+            response["Content-Type"] = f"image/{ext}"
             response["Content-Disposition"] = "inline"
             return response
         except IOError:
