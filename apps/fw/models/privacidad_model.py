5e5e9dda527d2cbdbec3a6789e3dcf95713c0a72
NDeleo <n.deleo94@gmail.com>
2023-06-20 20:31:02 -0300

    Se agrega verbose

--- apps/fw/models/privacidad_model.py
+++ apps/fw/models/privacidad_model.py
@@ -3,49 +3,53 @@ from django.db import models
 
 class Privacidad(models.Model):
     dni = models.BooleanField(
-        verbose_name="Numero de documento",
+        verbose_name="Mostrar numero de documento",
         default=True,
     )
     email = models.BooleanField(
-        verbose_name="Correo Electronico",
+        verbose_name="Mostrar correo electronico",
         default=True,
     )
     nombres = models.BooleanField(
-        "Nombres",
+        verbose_name="Mostrar nombres",
         default=True,
     )
     apellidos = models.BooleanField(
-        "Apellidos",
+        verbose_name="Mostrar apellidos",
         default=True,
     )
     fecha_nac = models.BooleanField(
-        "Fecha de Nacimiento",
+        verbose_name="Mostrar fecha de nacimiento",
         default=True,
     )
     nacionalidad = models.BooleanField(
-        "Nacionalidad",
+        verbose_name="Mostrar nacionalidad",
         default=True,
     )
     ciudad_natal = models.BooleanField(
-        "Ciudad natal",
+        verbose_name="Mostrar ciudad natal",
         default=True,
     )
     ciudad_actual = models.BooleanField(
-        "Ciudad actual",
+        verbose_name="Mostrar ciudad actual",
         default=True,
     )
     domicilio = models.BooleanField(
-        "Domicilio",
+        verbose_name="Mostrar domicilio",
         default=False,
     )
     certificado = models.BooleanField(
-        "Certificado",
+        verbose_name="Mostrar certificado",
         default=False,
     )
     sexo = models.BooleanField(
-        "Sexo",
+        verbose_name="Mostrar sexo",
         default=True,
     )
     egresado = models.ForeignKey(
         "FwUser", related_name="privacidad", on_delete=models.CASCADE
     )
+
+    class Meta:
+        verbose_name = "Privacidad"
+        verbose_name_plural = "Privacidades"
