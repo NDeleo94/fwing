86bb8497ae624965cf82e4479e092c1425775996
NDeleo <n.deleo94@gmail.com>
2023-06-24 01:29:12 -0300

    Se agregan las rutas para las apiviews

--- apps/fw/uris/egresado_urls.py
+++ apps/fw/uris/egresado_urls.py
@@ -5,17 +5,27 @@ from apps.fw.api.egresado_api import *
 urlEgresado = [
     path(
         "api/egresados/",
-        EgresadoAPIView.as_view({"get": "list"}),
+        EgresadoReadOnlyAPIView.as_view({"get": "list"}),
         name="egresado_list",
     ),
     path(
         "api/egresados/<int:pk>/",
-        EgresadoAPIView.as_view({"get": "retrieve"}),
+        EgresadoReadOnlyAPIView.as_view({"get": "retrieve"}),
         name="egresado_detail",
     ),
     path(
+        "api/egresados/",
+        EgresadoUpdateAPIView.as_view({"post": "create"}),
+        name="egresado_create",
+    ),
+    path(
         "api/egresados/<int:pk>/",
-        EgresadoAPIView.as_view({"put": "update"}),
+        EgresadoUpdateAPIView.as_view({"put": "update"}),
         name="egresado_update",
+    ),
+    path(
+        "api/egresados/<int:pk>/",
+        EgresadoUpdateAPIView.as_view({"delete": "destroy"}),
+        name="egresado_delete",
     ),
 ]
