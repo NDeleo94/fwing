a1f820f256dc52356317713afd4dab190b3e3a31
NDeleo <n.deleo94@gmail.com>
2023-06-27 12:46:43 -0300

    Se agregan apis de universidad

--- apps/fw/api/universidad_api.py
+++ apps/fw/api/universidad_api.py
@@ -1,18 +1,28 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 
 from apps.fw.serializers.universidad_serializers import *
+
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
+
+
+class UniversidadUpdateAPIView(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet,
+):
+    serializer_class = UniversidadReadOnlySerializer
+    queryset = serializer_class.Meta.model.objects.filter(estado=True)
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+
+class UniversidadReadOnlyAPIView(viewsets.ReadOnlyModelViewSet):
+    serializer_class = UniversidadReadOnlySerializer
+    queryset = serializer_class.Meta.model.objects.filter(estado=True)
 
 
 class UniversidadAPIView(viewsets.ModelViewSet):
     serializer_class = UniversidadSerializer
-    queryset = serializer_class.Meta.model.objects.filter(estado=True)
-
-
-class UniversidadListAPIView(viewsets.ReadOnlyModelViewSet):
-    serializer_class = UniversidadListSerializer
-    queryset = serializer_class.Meta.model.objects.filter(estado=True)
-
-
-class UniversidadDetailAPIView(viewsets.ReadOnlyModelViewSet):
-    serializer_class = UniversidadDetailSerializer
     queryset = serializer_class.Meta.model.objects.filter(estado=True)
