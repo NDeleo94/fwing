9b31b9119aa65d6bcb23f1a05c7720994e6c9cae
NDeleo <n.deleo94@gmail.com>
2023-06-27 13:06:10 -0300

    Se implementa borrado logico

--- apps/fw/api/universidad_api.py
+++ apps/fw/api/universidad_api.py
@@ -2,13 +2,15 @@ from rest_framework import viewsets, mixins
 
 from apps.fw.serializers.universidad_serializers import *
 
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
 
 
 class UniversidadUpdateAPIView(
     mixins.CreateModelMixin,
-    mixins.ListModelMixin,
+    mixins.UpdateModelMixin,
     mixins.DestroyModelMixin,
     viewsets.GenericViewSet,
 ):
@@ -16,6 +18,15 @@ class UniversidadUpdateAPIView(
     queryset = serializer_class.Meta.model.objects.filter(estado=True)
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.estado = False
+        instance.save()
+        return Response(
+            {"message": "Universidad deleted"},
+            status=status.HTTP_200_OK,
+        )
 
 
 class UniversidadReadOnlyAPIView(viewsets.ReadOnlyModelViewSet):
