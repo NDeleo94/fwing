20482bd7a1c03cc2f2df7ace50dd66e6631c1dc5
NDeleo <n.deleo94@gmail.com>
2023-06-27 23:55:50 -0300

    Se agrega apis de actualizacion y lectura para organizacion

--- apps/fw/api/organizacion_api.py
+++ apps/fw/api/organizacion_api.py
@@ -1,18 +1,39 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 
 from apps.fw.serializers.organizacion_serializers import *
+
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
+
+
+class OrganizacionUpdateAPIView(
+    mixins.CreateModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet,
+):
+    serializer_class = OrganizacionUpdateSerializer
+    queryset = serializer_class.Meta.model.objects.filter(estado=True)
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.estado = False
+        instance.save()
+        return Response(
+            {"message": "Organizacion deleted"},
+            status=status.HTTP_200_OK,
+        )
+
+
+class OrganizacionReadOnlyAPIView(viewsets.ReadOnlyModelViewSet):
+    serializer_class = OrganizacionReadOnlySerializer
+    queryset = serializer_class.Meta.model.objects.filter(estado=True)
 
 
 class OrganizacionAPIView(viewsets.ModelViewSet):
     serializer_class = OrganizacionSerializer
-    queryset = serializer_class.Meta.model.objects.filter(estado=True)
-
-
-class OrganizacionListAPIView(viewsets.ReadOnlyModelViewSet):
-    serializer_class = OrganizacionListSerializer
-    queryset = serializer_class.Meta.model.objects.filter(estado=True)
-
-
-class OrganizacionDetailAPIView(viewsets.ReadOnlyModelViewSet):
-    serializer_class = OrganizacionDetailSerializer
     queryset = serializer_class.Meta.model.objects.filter(estado=True)
