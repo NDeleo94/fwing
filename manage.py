7f8331605334a306d0868e1b69b471d79367c082
NDeleo <n.deleo94@gmail.com>
2023-07-03 13:14:48 -0300

    Se cambia configuracion

--- manage.py
+++ manage.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
-from dotenv import load_dotenv
 import os
 import sys
 
-load_dotenv()
+from decouple import config
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault(
-        "DJANGO_SETTINGS_MODULE", os.environ.get("DJANGO_SETTINGS_MODULE")
-    )
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", config("DJANGO_SETTINGS_MODULE"))
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
