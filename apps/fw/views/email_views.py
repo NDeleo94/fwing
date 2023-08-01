4d027008aded0fa8f76a4ff2aea70fb618cf3efa
NDeleo <n.deleo94@gmail.com>
2023-07-31 22:35:44 -0300

    Correciones

--- apps/fw/views/email_views.py
+++ apps/fw/views/email_views.py
@@ -37,50 +37,48 @@ class EmailView(APIView):
 class EmailTemplateView(APIView):
     def post(self, request):
         destinataries = request.data["usuarios"]
-        email_template = Email.objects.filter(id=request.data["plantilla"]).first()
 
-        # Replace the placeholder "@user" with the actual user's name in the email body.
+        email_template = Email.objects.filter(
+            id=request.data["plantilla"],
+        ).first()
+
+        if not email_template:
+            return Response({"message": "Template not found"})
+
         body_with_user = email_template.cuerpo.replace("@nombres", "{nombres}").replace(
             "@apellidos", "{apellidos}"
         )
 
-        # Loop through the list of user IDs and send the email to each user.
+        counter = 0
         for user_id in destinataries:
             user = get_object_or_404(FwUser, id=user_id)
             subject = email_template.asunto
-            from_email = "egresados.computacion@herrera.unt.edu.ar"  # Update with your sender email
-            to_email = "n.deleo94@gmail.com"
+            from_email = settings.EMAIL_HOST_USER
+            to_email = "n.deleo94@gmail.com"  # user.email
 
-            # Replace "{name}" with the user's name in the email body.
-            personalized_body = body_with_user.format(
-                nombres=user.nombres, apellidos=user.apellidos
+            body = body_with_user.format(
+                nombres=user.nombres,
+                apellidos=user.apellidos,
             )
 
-            text_content = "Hi {nombres}, Welcome to My System!".format(
-                nombres=user.nombres, apellidos=user.apellidos
+            text_content = "Hi {nombres}, Welcome to Following!".format(
+                nombres=user.nombres,
+                apellidos=user.apellidos,
             )
-            html_content = personalized_body
 
-            msg = EmailMultiAlternatives(subject, text_content, from_email, [to_email])
-            msg.attach_alternative(html_content, "text/html")
+            html_content = body
+
+            msg = EmailMultiAlternatives(
+                subject,
+                text_content,
+                from_email,
+                [to_email],
+            )
+            msg.attach_alternative(
+                html_content,
+                "text/html",
+            )
             msg.send()
-        # template = get_template("correo_invitacion.html")
-        # content = template.render()
+            counter += 1
 
-        # email = EmailMultiAlternatives(
-        #     "Correo de prueba",
-        #     "FollowING",
-        #     settings.EMAIL_HOST_USER,
-        #     [destinatary],
-        # )
-        # email.attach_alternative(
-        #     content=content,
-        #     mimetype="text/html",
-        # )
-
-        # sended = email.send()
-        # if sended:
-        #     return Response("Email enviado")
-        # return Response("Error al enviar email")
-
-        return Response({"message": "Emails sent successfully!"})
+        return Response({"message": f"{counter} Emails sent successfully!"})
