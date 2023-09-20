9fc88be53db127b4231d4d5139ec10559f6a4b41
NDeleo <n.deleo94@gmail.com>
2023-09-20 00:07:38 -0300

    Se agrega modelo log de siu

---
+++ apps/fw/models/log_siu_model.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+
+class LogSIU(models.Model):
+    id = models.AutoField(primary_key=True)
+    cantidad = models.IntegerField(
+        "Cantidad de egresados",
+        blank=False,
+        null=False,
+    )
+    origen = models.IntegerField(
+        "Actualizado desde",
+        choices=(
+            (1, "AUTO"),
+            (2, "MANUAL"),
+        ),
+        blank=False,
+        null=False,
+    )
+    fecha = models.DateField(
+        "Fecha de Actualización",
+        auto_now_add=True,
+        blank=False,
+        null=False,
+    )
+
+    class Meta:
+        verbose_name = "Log de SIU"
+        verbose_name_plural = "Log de SIU"
