335ef6e3d3f939a7054cefb5c138c2d9b649a1d3
NDeleo <n.deleo94@gmail.com>
2023-10-08 13:40:49 -0300

    Se agregan funciones de privacidad

--- apps/fw/utils/privacidad_utils.py
+++ apps/fw/utils/privacidad_utils.py
@@ -1,5 +1,7 @@
 from apps.fw.models.privacidad_model import Privacidad
 
+from apps.fw.serializers.privacidad_serializers import PrivacidadSerializer
+
 
 def check_privacity(usuario):
     privacidad = Privacidad.objects.filter(
@@ -10,3 +12,13 @@ def check_privacity(usuario):
         return privacidad
     else:
         return False
+
+
+def create_privacidad(usuario):
+    data_privacidad = {
+        "usuario": usuario.id,
+    }
+    serializer = PrivacidadSerializer(data=data_privacidad)
+    serializer.is_valid(raise_exception=True)
+    privacidad = serializer.save()
+    return privacidad
