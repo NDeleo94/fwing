6152de813466a21b0c6d89f20abbd502ac177276
NDeleo <n.deleo94@gmail.com>
2023-10-08 14:06:02 -0300

    Refactorizacion de utils de universidad

--- apps/fw/utils/universidad_utils.py
+++ apps/fw/utils/universidad_utils.py
@@ -3,16 +3,31 @@ from apps.fw.models.universidad_model import Universidad
 from apps.fw.serializers.universidad_serializers import *
 
 
-def get_or_create_universidad(data):
-    if type(data) is str:
-        data_cased = data.title()
-        data_universidad = {
-            "universidad": data_cased,
-        }
-        serializer = UniversidadUpdateSerializer(data=data_universidad)
-        serializer.is_valid(raise_exception=True)
-        universidad = serializer.save()
+def create_universidad(data):
+    data_cased = data.title()
+    data_universidad = {
+        "universidad": data_cased,
+    }
+    serializer = UniversidadUpdateSerializer(data=data_universidad)
+    serializer.is_valid(raise_exception=True)
+    universidad = serializer.save()
+
+    return universidad
+
+
+def get_universidad(data):
+    if not data:
+        universidad = None
     else:
         universidad = Universidad.objects.get(id=data)
+
+    return universidad
+
+
+def get_or_create_universidad(data):
+    universidad = get_universidad(data=data)
+
+    if not universidad:
+        universidad = create_universidad(data=data)
 
     return universidad
