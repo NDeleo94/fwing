038a243009e3d42b441af96dc9c5378fcd8da258
NDeleo <n.deleo94@gmail.com>
2023-10-08 14:06:27 -0300

    Refactorizacion de utils de facultad

--- apps/fw/utils/facultad_utils.py
+++ apps/fw/utils/facultad_utils.py
@@ -3,17 +3,35 @@ from apps.fw.models.facultad_model import Facultad
 from apps.fw.serializers.facultad_serializers import *
 
 
-def get_or_create_facultad(data, universidad):
-    if type(data) is str:
-        data_cased = data.title()
-        data_facultad = {
-            "facultad": data_cased,
-            "universidad": universidad,
-        }
-        serializer = FacultadUpdateSerializer(data=data_facultad)
-        serializer.is_valid(raise_exception=True)
-        facultad = serializer.save()
+def create_facultad(data, universidad_id):
+    data_cased = data.title()
+    data_facultad = {
+        "facultad": data_cased,
+        "universidad": universidad_id,
+    }
+    serializer = FacultadUpdateSerializer(data=data_facultad)
+    serializer.is_valid(raise_exception=True)
+    facultad = serializer.save()
+
+    return facultad
+
+
+def get_facultad(data):
+    if not data:
+        facultad = None
     else:
         facultad = Facultad.objects.get(id=data)
+
+    return facultad
+
+
+def get_or_create_facultad(data, universidad_id):
+    facultad = get_facultad(data=data)
+
+    if not facultad:
+        facultad = create_facultad(
+            data=data,
+            universidad_id=universidad_id,
+        )
 
     return facultad
